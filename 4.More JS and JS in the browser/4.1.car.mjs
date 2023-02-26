a48fed14186f822c1e4575cf6802e61ae95c171d
Merve Balci <mervebalci090@gmail.com>
2023-02-26 00:07:49 -0400

    ES Modules

---
+++ 4.More JS and JS in the browser/4.1.car.mjs
@@ -0,0 +1,9 @@
+const drive = () => {
+    console.log('drive')
+}
+
+const park = () => {
+    console.log('park')
+}
+
+export { drive, park }
---
+++ 4.More JS and JS in the browser/4.2.test.mjs
@@ -0,0 +1,6 @@
+import { drive, park } from './4.1.car.mjs'
+
+drive()
+park()
+
+// If we don't save the file name as .mjs, code gives "SyntaxError: Cannot use import statement outside a module"
