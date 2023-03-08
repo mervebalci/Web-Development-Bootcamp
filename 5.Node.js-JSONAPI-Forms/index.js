3824aefd790b7b6c7c33103732fb538a51380b23
Merve Balci <mervebalci090@gmail.com>
2023-03-08 11:59:35 -0400

    Introduction to Node.js

---
+++ 5.Node.js-JSONAPI-Forms/index.js
@@ -0,0 +1,11 @@
+// Let's try if code will be executed:
+console.log('test')
+
+/*
+Now that JavaScript is running outside of the browser, it can do many more things, 
+because we’re running it outside of the browser sandbox, 
+a security tool that prevents your computer to be hacked all the time just by visiting random websites.
+
+JavaScript in the browser is limited. 
+But outside of it, it can access the network, the filesystem, the database, configuration options, and a lot more.
+*/
