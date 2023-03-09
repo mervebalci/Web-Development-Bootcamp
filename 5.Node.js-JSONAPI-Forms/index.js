61c61797656018b8cf574ea0ab8db466c3e75e31
Merve Balci <mervebalci090@gmail.com>
2023-03-09 11:09:48 -0400

    Writing the first Node.js code and Importing files

--- 5.Node.js-JSONAPI-Forms/index.js
+++ 5.Node.js-JSONAPI-Forms/index.js
@@ -1,5 +1,5 @@
 // Let's try if code will be executed:
-console.log('test')
+// console.log('test')
 
 /*
 Now that JavaScript is running outside of the browser, it can do many more things, 
@@ -8,4 +8,42 @@ a security tool that prevents your computer to be hacked all the time just by vi
 
 JavaScript in the browser is limited. 
 But outside of it, it can access the network, the filesystem, the database, configuration options, and a lot more.
+*/
+
+
+// Using this new way, instead of using 'require' we’ll use 'IMPORT':
+import { sum } from './lib.js'
+console.log(sum(13, 19))
+
+
+// Calling the lib.js with REQUIRE function:
+// const { sum } = require('./lib.js')
+// console.log(sum(13, 19))
+
+// This is the Node.js require syntax, also called CommonJS.
+// It’s been the way to import files and modules in Node.js for years.
+
+// Lately, there’s been a new way which is ES Modules.
+// Using this new way, instead of using require we’ll use import:
+// import { sum } from './lib.js'
+
+
+/*
+This error is showed in the terminal while importing a function from a JavaScript file.
+SyntaxError: cannot use import statement outside a module
+
+This error occurs for one reason: you’re trying to use import and you’re not inside an ES module.
+It can happen in a Node.js environment, or in the browser.
+
+First, we need to tell Node.js that this is a module.
+So, add package.json file in the folder of the project and add this below code:
+{
+    "type": "module"
+}
+
+Things are now working because we told Node.js that this is a module and should use ES modules syntax.
+
+
+In the browser instead, you have to add the type attribute with the value module when you load the script:
+<script type="module" src="./file.js"></script>
 */
---
+++ 5.Node.js-JSONAPI-Forms/lib.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+    return a + b
+}
+
+// Exporting the function to be REQUIRED in index.js file:
+// exports.sum = sum
+
+// Exporting the function to be IMPORTED in index.js file:
+export { sum }
