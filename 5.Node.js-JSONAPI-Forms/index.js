0c85496f1b81fda616297c8514dc99c0da8a77c9
Merve Balci <mervebalci090@gmail.com>
2023-03-11 19:56:48 -0400

    Built-in Modules

--- 5.Node.js-JSONAPI-Forms/index.js
+++ 5.Node.js-JSONAPI-Forms/index.js
@@ -47,3 +47,25 @@ Things are now working because we told Node.js that this is a module and should 
 In the browser instead, you have to add the type attribute with the value module when you load the script:
 <script type="module" src="./file.js"></script>
 */
+
+
+
+
+// Using RENAME method of 'fs' built-in module:
+import fs from 'fs'
+
+fs.rename('file.txt', 'newfilename.txt', (err) => {
+    if (err) {
+        // there's been an error of renaming!
+        console.log(err)
+        return
+    }
+    // the file has been renamed!
+})
+
+/*
+This would be impossible inside the browser. 
+JavaScript has no access to files on the computer, otherwise, it’d be a security risk. 
+But Node.js programs can access the filesystem.
+It’s not like the program runs automatically when you visit a random website.
+*/
