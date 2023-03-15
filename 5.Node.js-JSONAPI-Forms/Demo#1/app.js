2ad4d711a1b0d6b34af0cbae1e5744416af33f0c
Merve Balci <mervebalci090@gmail.com>
2023-03-14 21:49:17 -0300

    Managing the form submit event

--- 5.Node.js-JSONAPI-Forms/Demo#1/app.js
+++ 5.Node.js-JSONAPI-Forms/Demo#1/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+// Wrapping all the code into DOM:
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const username = document.querySelector('input').value
+        alert(username)
+    })
+})
