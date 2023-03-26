b1f16d7decf6a18fa410274aee810da59149567a
Merve Balci <mervebalci090@gmail.com>
2023-03-26 18:48:33 -0300

    Adding a second page to the site and Linking them

--- 8.nextjs/pages/index.js
+++ 8.nextjs/pages/index.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <Link href='/blog'>Blog</Link>
     </div>
   )
 }
