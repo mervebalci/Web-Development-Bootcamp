3a5eef08681f9cf951c34c5e347799786dc6ef77
Merve Balci <mervebalci090@gmail.com>
2023-03-30 23:42:35 -0300

    Static data fetching at build time

--- 8.nextjs/pages/blog/[id].js
+++ 8.nextjs/pages/blog/[id].js
@@ -30,19 +30,54 @@ In particular we can get the dynamic part of the URL in the `[id].js` file by ac
 The dynamic part can also just be a portion of the URL, like post-[id].js.
 */
 
-import { useRouter } from 'next/router'
+
+/*
+Data fetching when a user visits a page, both in the backend and in the frontend.
+Next.js also offers another way. It’s called static data fetching.
+
+Suppose you have a blog. You have a set of blog posts, perhaps published on a service like Contentful or Sanity. 
+Or on a headless Wordpress install.
+You can tell Next.js to fetch that content at build time, 
+and generate static pages that are then served to the user without further action.
+It’s the best of both worlds: your data is dynamic in nature, but you create static pages from it.
+
+How does it work?
+You have to define and export 2 functions in your page component:
+    - `getStaticPaths`
+    - `getStaticProps`
+
+The first defines the dynamic URLs that the page allows.
+
+Object.keys() is a way to convert an object's properties to an array, 
+so then you can iterate over it using map().
+
+Now the page component receives the post parameter as its prop, 
+and it does not need to load a router and do any kind of client-side data lookup, 
+because Next.js does this at build time:
+*/
+
 import posts from 'posts.json'
 
-export default function BlogPost() {
-  const router = useRouter()
-
-  const post = posts[router.query.id]
-  if (!post) return <p></p>
-
+export default function BlogPost({ post }) {
   return (
     <>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
     </>
   )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: Object.keys(posts).map((id) => ({ params: { id } })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      post: posts[params.id],
+    },
+  }
 }
